c20600214a99d0d8b1ca6c9ebb67c022c1a18a47
Li Junchen <lijunchen95@outlook.com>
2023-09-14 15:37:33 +0800

    helloworld v0.0.1

--- helloworld/lib.go
+++ helloworld/lib.go
@@ -1,17 +1,13 @@
-package main
+package helloworld
 
 import (
-	"fmt"
-
 	"github.com/lijunchen/gomod/character"
 	"github.com/lijunchen/gomod/character/space"
 	"github.com/lijunchen/gomod/character/symbol"
-
 	"github.com/lijunchen/gomod/hello"
 	"github.com/lijunchen/gomod/world"
 )
 
-func main() {
-	s := character.FirstToUpper(hello.Hello()) + symbol.Comma() + space.Space() + world.World() + symbol.Exclamation()
-	fmt.Println(s)
+func HelloWorld() string {
+	return character.FirstToUpper(hello.Hello()) + symbol.Comma() + space.Space() + world.World() + symbol.Exclamation()
 }
---
+++ helloworld/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lijunchen/gomod/helloworld"
+)
+
+func main() {
+	fmt.Println(helloworld.HelloWorld())
+}
